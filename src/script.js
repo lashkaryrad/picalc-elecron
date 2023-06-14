6dfee0f4e8fc0e3479aaa5051d6173eb39296420
lashkaryrad <lashkaryrad@gmail.com>
2023-06-14 16:19:49 +0330

    make instrument works

--- src/script.js
+++ src/script.js
@@ -1,20 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("form").onsubmit = () => {
 
+        // fetch data from server
         fetch('https://api.freecurrencyapi.com/v1/latest?apikey=e4HSy2o8cJ2gzT2u4u61kvohmyTJKEAHfdi6Ndeg')
+        // get response with json
         .then((response) => response.json())
+        // get response as data
         .then(data => {
 
-            var currency = document.getElementById('currency').value.toUpperCase();
+
+            // var pip : pips_count
+            // var instrument : instrument
+            // var lot : lot
+            // var deposit_currency = deposit_currency
+
+            var instrument = document.getElementById('instrument').value.toUpperCase();
+
             var lot = document.getElementById('lot').value;
+
             var frm = document.querySelector('form');
-            console.log(currency);
+
+            console.log(instrument);
             
-            var price = data["data"][currency];
+            var price = data["data"][instrument];
 
             var pip = (0.0001 / price) * lot
 
-            document.getElementById("show-pip").innerHTML = `$${pip.toFixed(2)}`;
+            alert( `$${pip.toFixed(2)}`)
+            // document.getElementById("show-pip").innerHTML = `$${pip.toFixed(2)}`;
             frm.reset()
         })
         .catch(error => {
